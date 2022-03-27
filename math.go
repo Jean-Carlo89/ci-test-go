43213bcb11925ca2655f90156aa225e50b5179a3
Jean Carlo <jeancarlomo@hotmail.com>
2022-03-27 11:33:37 -0300

    test

--- math.go
+++ math.go
@@ -5,6 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println(soma(10, 20))
 	fmt.Println("test")
+
+	bi := [][]int{
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+		{8, 9, 10, 11},
+	}
+
+	fmt.Println(bi[1][1])
 }
 
 func soma(a int, b int) int {
